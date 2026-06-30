b99767d55f6898b14f0b79c9506b12a32ad39527
SaffronWare <aryanshadzi@gmail.com>
2026-06-30 00:10:18 -0400

    it works1

--- VizMa/VizMa.hpp
+++ VizMa/VizMa.hpp
@@ -14,20 +14,14 @@ namespace vzm {
 		unsigned int window_size = 0u;
 		unsigned int window_height = 0u;
 		float dt = 0.0f;
-		float time = 0.0f; // 1st 16 byte chunk of memory
+		float time = 0.0f; 
 
 		SDL_Window* window = nullptr;
 		SDL_Surface* window_surface = nullptr; // 2nd 16 byte chunk
 
-		const char* label;
-		const SDL_PixelFormatDetails* format_data = nullptr;
-
-
-		SDL_PixelFormat format = nullptr;
-		
-		const char* format_name;
 		unsigned int flags;
 		unsigned int frame;
+		const char* label;
 		
 	public:
 	
@@ -40,6 +34,7 @@ namespace vzm {
 		~Project()
 		{
 			SDL_DestroyWindow(window);
+			SDL_Quit();
 			window = nullptr;
 			window_surface = nullptr;
 		}
@@ -47,44 +42,47 @@ namespace vzm {
 		void start(unsigned int window_width = 1000, unsigned int window_height = 800)
 		{
 
+			SDL_Init(SDL_INIT_VIDEO);
+
 			window = SDL_CreateWindow(label, window_width, window_height, 0);
-
-			SDL_Surface* window_surface = SDL_GetWindowSurface(window);
-
-
+			window_surface = SDL_GetWindowSurface(window);
 
 			DerivedProject* imp_this = static_cast<DerivedProject*>(this);
+			unsigned int pixel_pitch = window_surface->pitch / sizeof(Uint32);
+		
+			Uint32* pixel_buffer = static_cast<Uint32*>(window_surface->pixels);
+			Uint32* buffer_position;
 
-			format = SDL_GetWindowPixelFormat(window);
-			format_data = SDL_GetPixelFormatDetails(format);
-			format_name = SDL_GetPixelFormatName(format);
-			
-
-			unsigned int pixel_stride = format->bits_per_pixel;
-
-			std::cout << format->size << std::endl;
+			SDL_Event event;
 			while (true)
 			{
+				while (SDL_PollEvent(&event))
+				{
+					if (event.type == SDL_EVENT_QUIT)
+					{
+						return;
+					}
+				}
 
-				
 				SDL_LockSurface(window_surface);
 
-				// ignore for now just to test template
-				unsigned int buffer_position = 0;
-				for (int i = 0; i < window_width; i++)
+				buffer_position = pixel_buffer;
+
+				for (int j = 0; j < window_height; j++)
 				{
-					for (int j = 0; j < window_height; j++)
+					for (int i = 0; i < window_width; i++)
 					{
 						
-
-						window_surface->pixels[buffer_position] = imp_this->render_pixel(i, j);
-
-						buffer_position += pixel_stride;
+						buffer_position[i] = imp_this->render_pixel(i, j);
+						
 					}
-					
+
+					buffer_position += pixel_pitch;
 				}
 				
 				SDL_UnlockSurface(window_surface);
+
+				SDL_UpdateWindowSurface(window);
 
 				frame++;
 			}
--- testing.cpp
+++ testing.cpp
@@ -8,13 +8,15 @@ struct TestProject : public vzm::Project<TestProject>
 
 	inline Uint32 render_pixel(unsigned int x, unsigned int y)
 	{
-		return 0xFFFFFFFF;
+		return 0xFFFF0000;
 
 	}
 };
 
 int main()
 {
+	std::cout << "Starting TestProject..." << std::endl;
+
 	TestProject project = TestProject();
 	project.start();
 
